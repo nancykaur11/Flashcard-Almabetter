26ecea3ae22aaf65b6088829caa02c98088fdf7c
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-09 03:12:14 +0530

    Link fixed

--- src/page/CardsViewer.js
+++ src/page/CardsViewer.js
@@ -1,9 +1,9 @@
 import { BiArrowBack } from "react-icons/bi";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RiArrowGoBackLine } from "react-icons/ri";
-import ShareModal from "../components/design/ShareModal";
+// import ShareModal from "../components/design/ShareModal";
 import { IoDownloadOutline, IoPrintOutline } from "react-icons/io5";
 
 const CardsViewer = () => {
@@ -14,22 +14,20 @@ const CardsViewer = () => {
   const [activeGroup, setActiveGroup] = useState({});
   const [activeCard, setActiveCard] = useState([]);
   const cards = useSelector((state) => state.cards);
-  console.log(cards)
-
+  console.log(cards);
 
   const handleDownload = () => {
     const currentView = document.documentElement.outerHTML; // Get the entire HTML of the current view
-    const blob = new Blob([currentView], { type: 'text/html' }); // Create a Blob with the HTML content
+    const blob = new Blob([currentView], { type: "text/html" }); // Create a Blob with the HTML content
     const url = URL.createObjectURL(blob); // Create a URL for the Blob
-  
-    const a = document.createElement('a'); // Create an <a> element
+
+    const a = document.createElement("a"); // Create an <a> element
     a.href = url; // Set the URL as the href attribute
-    a.download = 'current-view.html'; // Set the desired download filename
+    a.download = "current-view.html"; // Set the desired download filename
     a.click(); // Programmatically trigger a click event on the <a> element
-  
+
     URL.revokeObjectURL(url); // Clean up the URL object
   };
-  
 
   useEffect(() => {
     const foundCard = cards.find((item) => item.groups.group_Id === group_Id);
@@ -47,9 +45,13 @@ const CardsViewer = () => {
   return (
     <section className="flex flex-col text-slate-600">
       <header className="flex">
-        <BiArrowBack className="text-3xl mr-6 cursor-pointer" />
+        <Link to="/falshcards">
+          <BiArrowBack className="text-3xl mr-6 cursor-pointer" />
+        </Link>
         <div className="flex flex-col">
-          <h2 className="text-xl text-black font-bold">{activeGroup?.groups?.group}</h2>
+          <h2 className="text-xl text-black font-bold">
+            {activeGroup?.groups?.group}
+          </h2>
           <p className="my-2">{activeGroup?.groups?.groupDesc}</p>
         </div>
       </header>
@@ -58,29 +60,32 @@ const CardsViewer = () => {
           <h2 className="p-2">Flashcards</h2>
           <hr />
           <hr className="mb-2" />
-          {console.log(activeGroup.terms,'d')}
-          {activeGroup.terms && activeGroup?.terms.map((d)=>{ 
-          <img
-          alt="cardimage"
-          src={d.image} 
-          className="object-contain w-[32rem] xl:w-[20vw] h-full p-6"
-        />
-          {console.log(d.image, d.term,'dd')}
-          })}
-          <p className={`py-2 px-8 text-slate-700 font-medium hover:bg-slate-100 cursor-pointer`}>
-            
-          </p>
+          {console.log(activeGroup.terms, "d")}
+          {activeGroup.terms &&
+            activeGroup?.terms.map((d) => {
+              <img
+                alt="cardimage"
+                src={d.image}
+                className="object-contain w-[32rem] xl:w-[20vw] h-full p-6"
+              />;
+              {
+                console.log(d.image, d.term, "dd");
+              }
+            })}
+          <p
+            className={`py-2 px-8 text-slate-700 font-medium hover:bg-slate-100 cursor-pointer`}
+          ></p>
         </aside>
         <section className="col-span-3 md:col-span-2 flex flex-col xl:flex-row items-center w-full bg-white shadow-lg rounded-lg">
           <img
             alt="cardimage"
-            src={activeGroup?.groups?.Profile} 
+            src={activeGroup?.groups?.Profile}
             className="object-contain w-[32rem] xl:w-[20vw] h-full p-6"
           />
           <p className={`w-full p-6 py-10 `}>Card Description</p>
         </section>
         <aside className="col-span-1 hidden md:flex flex-col items-center space-y-5">
-        <button
+          <button
             type="button"
             onClick={openModal}
             className="flex items-center py-3 px-4 xl:w-60 space-x-5 bg-white rounded-md shadow-lg active:scale-100 transition-all duration-100 hover:scale-105"
@@ -89,23 +94,23 @@ const CardsViewer = () => {
             <span className="hidden xl:block">Share</span>
           </button>
           <button
-  className="flex items-center py-3 px-4 xl:w-60 space-x-5 bg-white rounded-md shadow-lg active:scale-100 transition-all duration-100 hover:scale-105"
-  onClick={handleDownload}
->
-  <IoDownloadOutline />
-  <span className="hidden xl:block">Download</span>
-</button>
+            className="flex items-center py-3 px-4 xl:w-60 space-x-5 bg-white rounded-md shadow-lg active:scale-100 transition-all duration-100 hover:scale-105"
+            onClick={handleDownload}
+          >
+            <IoDownloadOutline />
+            <span className="hidden xl:block">Download</span>
+          </button>
 
           <button
-  className="flex items-center py-3 px-4 xl:w-60 space-x-5 bg-white rounded-md shadow-lg active:scale-100 transition-all duration-100 hover:scale-105"
-  onClick={() => window.print()}
->
-  <IoPrintOutline />
-  <span className="hidden xl:block">Print</span>
-</button>
+            className="flex items-center py-3 px-4 xl:w-60 space-x-5 bg-white rounded-md shadow-lg active:scale-100 transition-all duration-100 hover:scale-105"
+            onClick={() => window.print()}
+          >
+            <IoPrintOutline />
+            <span className="hidden xl:block">Print</span>
+          </button>
         </aside>
       </main>
-      <ShareModal isOpen={isOpen} closeModal={closeModal} />
+      {/* <ShareModal isOpen={isOpen} closeModal={closeModal} /> */}
     </section>
   );
 };
