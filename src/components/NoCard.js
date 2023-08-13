e35789e5037fe66ce6dbdf98a60012c8824aff5a
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-13 12:56:25 +0530

    error handling completed

---
+++ src/components/NoCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const NoCard = () => {
+  const theme = useSelector((state) => state.theme);
+
+  return (
+    <div
+      className={`h-[80vh] flex justify-center flex-col items-center ${
+        theme ? "bg-white text-black" : "bg-slate-900 text-white"
+      }`}
+    >
+      <h6 className="text-3xl mb-10">There is no card Please create new Card</h6>
+      <Link to={"/"}>
+        <button className="border-2 border-red-700 hover:bg-red-700 hover:text-white px-2 py-2 rounded-lg transition-all duration-500">Create new Card</button>
+      </Link>
+    </div>
+  );
+};
+
+export default NoCard;
--- src/page/Flashcards.js
+++ src/page/Flashcards.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { remove } from "../store/features/cards";
 import { useDispatch } from "react-redux";
+import NoCard from "../components/NoCard";
 
 const Flashcards = () => {
   const cards = useSelector((state) => state.cards);
@@ -11,41 +12,47 @@ const Flashcards = () => {
   const dispatch = useDispatch();
 
   return (
-    <div
-      className={`pt-12 transition-all min-h-screen duration-700 ${
-        theme ? "bg-white-100 tex-black" : "bg-gray-900 text-white"
-      }`}
-    >
-      <div className="auto flex flex-wrap w-full justify-around items-center px-[10%]">
-        {cards.map(({ groups, terms }, index) => (
-          <div
-            key={index}
-            className={`w-[400px] relative h-56 mt-6 rounded-lg transition-all duration-700 ${
-              theme
-                ? "shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
-                : "shadow-2xl shadow-blue-500/20"
-            } hover:shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]`}
-          >
-            <AiFillCloseCircle
-              className={`absolute right-2 top-2 cursor-pointer text-2xl`}
-              onClick={() => dispatch(remove(groups.group_Id))}
-            />
-            <img
-              src={groups.Profile}
-              className="absolute w-20 h-20 left-1/2 -translate-x-10 -top-10 rounded-full shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]"
-            />
-            <h3 className="text-center mt-11">{groups.group}</h3>
-            <p className="text-center">{groups.groupDesc}</p>
-            <p className="text-center mt-5">{terms.length} Cards</p>
-            <Link to={`/flashcards/${groups.group_Id}/${terms[0].card_Id}`}>
-              <button className="absolute -translate-x-1/2 left-1/2 border-2 border-red-700 px-3 bottom-3 rounded-md">
-                View Cards
-              </button>
-            </Link>
+    <>
+      {cards.length === 0 ? (
+        <NoCard />
+      ) : (
+        <div
+          className={`pt-12 transition-all min-h-screen duration-700 ${
+            theme ? "bg-white-100 tex-black" : "bg-gray-900 text-white"
+          }`}
+        >
+          <div className="auto flex flex-wrap w-full justify-around items-center px-[10%]">
+            {cards.map(({ groups, terms }, index) => (
+              <div
+                key={index}
+                className={`w-[400px] relative h-56 mt-6 rounded-lg transition-all duration-700 ${
+                  theme
+                    ? "shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
+                    : "shadow-2xl shadow-blue-500/20"
+                } hover:shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]`}
+              >
+                <AiFillCloseCircle
+                  className={`absolute right-2 top-2 cursor-pointer text-2xl`}
+                  onClick={() => dispatch(remove(groups.group_Id))}
+                />
+                <img
+                  src={groups.Profile}
+                  className="absolute w-20 h-20 left-1/2 -translate-x-10 -top-10 rounded-full shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]"
+                />
+                <h3 className="text-center mt-11">{groups.group}</h3>
+                <p className="text-center">{groups.groupDesc}</p>
+                <p className="text-center mt-5">{terms.length} Cards</p>
+                <Link to={`/flashcards/${groups.group_Id}/${terms[0].card_Id}`}>
+                  <button className="absolute -translate-x-1/2 left-1/2 border-2 border-red-700 px-3 bottom-3 rounded-md">
+                    View Cards
+                  </button>
+                </Link>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
